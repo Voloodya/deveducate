d34cd26ca9c2b44898acfb16eb90dbf77072a652
DESKTOP-3DLKON3\vlutsenko <vlutsenko@cinimex.ru>
2021-11-02 16:52:13 +0500

    Create empty project

---
+++ src/main/java/com/example/deveducate/DeveducateApplication.java
@@ -0,0 +1,13 @@
+package com.example.deveducate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveducateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveducateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deveducate/DeveducateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deveducate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveducateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
