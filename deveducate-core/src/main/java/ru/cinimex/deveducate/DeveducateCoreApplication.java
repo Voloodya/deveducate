b16e6971df6962b1460fe18c312457cf1728e7ed
DESKTOP-3DLKON3\vlutsenko <vlutsenko@cinimex.ru>
2021-11-03 10:39:20 +0500

    Rebuilt the project

---
+++ deveducate-core/src/main/java/ru/cinimex/deveducate/DeveducateCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.cinimex.deveducate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveducateCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveducateCoreApplication.class, args);
+    }
+
+}
--- deveducate-core/src/test/java/ru/cinimex/deveducate/DeveducateCoreApplicationTests.java
+++ deveducate-core/src/test/java/ru/cinimex/deveducate/DeveducateCoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.deveducate;
+package ru.cinimex.deveducate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeveducateApplicationTests {
+class DeveducateCoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
